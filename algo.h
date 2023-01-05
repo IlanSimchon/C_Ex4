11cf9234e99f58f33402db88027b38c878089ff6
Ilan <smilan273@gmail.com>
2023-01-05 14:21:45 +0200

    init project

---
+++ algo.h
@@ -0,0 +1,8 @@
+//
+// Created by 97253 on 05/01/2023.
+//
+
+#ifndef C_EX4_ALGO_H
+#define C_EX4_ALGO_H
+
+#endif //C_EX4_ALGO_H
---
+++ edges.h
@@ -0,0 +1,8 @@
+//
+// Created by 97253 on 05/01/2023.
+//
+
+#ifndef C_EX4_EDGES_H
+#define C_EX4_EDGES_H
+
+#endif //C_EX4_EDGES_H
